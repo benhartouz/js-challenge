2e2fdded8e9b2f1593850976a856708649c07635
MOHAMED BEN HARTOUZ <mohamed.x.ben.hartouz.-nd@disney.com>
2021-05-01 01:49:38 +0200

    fix(refacto): refacto and remove temp url

--- src/actions/near-earth/index.js
+++ src/actions/near-earth/index.js
@@ -1,9 +1,9 @@
 import { URL } from "../../constants/config";
 import { NearEarthAdapter } from "../../actions/near-earth/adapters";
-const TEMP_API = "http://demo8747629.mockable.io/";
+//const TEMP_API = "http://demo8747629.mockable.io/";
 const fetchNearEarth = () => {
   return {
-    route: TEMP_API,
+    route: URL,
     method: "get",
     adapter: NearEarthAdapter,
   };
