b47b3e74085a58e7596ce9d5a1b23bd6c385757e
MOHAMED BEN HARTOUZ <mohamed.x.ben.hartouz.-nd@disney.com>
2021-05-01 00:53:17 +0200

    feat(csv_export): user can save data as csv file

--- src/components/templates/near-earth/components/table.js
+++ src/components/templates/near-earth/components/table.js
@@ -7,28 +7,44 @@ import {
   Th,
   Td,
   TableCaption,
+  Stack,
+  Button,
 } from "@chakra-ui/react";
+import { CSVLink, CSVDownload } from "react-csv";
 
 const TableDisplay = ({ data }) => {
+  console.log("data", data);
+  const csvData = [
+    ["NEO Name", "Min Estimated Diameter (km)", "Max Estimated Diameter"],
+    ...data,
+  ];
+
   return (
-    <Table variant="simple">
-      <Thead>
-        <Tr>
-          <Th>NEO Name</Th>
-          <Th isNumeric>Min Estimated Diameter (km)</Th>
-          <Th isNumeric>Max Estimated Diameter</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data.map((item) => (
+    <>
+      <Stack spacing={5} direction="row" align="center" marginBottom={5}>
+        <Button colorScheme="teal" size="lg">
+          <CSVLink data={csvData}>Save as csv file</CSVLink>
+        </Button>
+      </Stack>
+      <Table variant="simple">
+        <Thead>
           <Tr>
-            <Td>{item[0]}</Td>
-            <Td>{parseFloat(item[1]).toFixed(2)}</Td>
-            <Td>{parseFloat(item[1]).toFixed(2)}</Td>
+            <Th>NEO Name</Th>
+            <Th isNumeric>Min Estimated Diameter (km)</Th>
+            <Th isNumeric>Max Estimated Diameter</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr>
+              <Td>{item[0]}</Td>
+              <Td>{parseFloat(item[1]).toFixed(2)}</Td>
+              <Td>{parseFloat(item[1]).toFixed(2)}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
   );
 };
 
